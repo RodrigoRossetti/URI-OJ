67e8f33d563da5dc20d7daf499ea446d5ad2b6f7
RodrigoRossetti <aracknock@gmail.com>
2020-07-10 18:08:00 -0300

    2540

--- Volume 25 (2500 - 2599)/2540/2540.c
+++ Volume 25 (2500 - 2599)/2540/2540.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    float n,total = 0, vote;
-    while(scanf("%f",&n) != EOF ){
-        int i;
-        for(i = 0; i<n; i++){
-            scanf("%f",&vote);
-            if(vote == 1){
-                total++;
-            }
+    int n, voto;
+    while (scanf("%d", &n) != EOF ) {
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &voto);
+            if (voto) total++;
         }
-        if(total >= ((n/3)*2)){
-            printf("impeachment\n");
-        }
-        else{
-            printf("acusacao arquivada\n");
-        }
-        i = 0, n = 0, vote = 0, total = 0;
+        if (total >= (double)n/3*2) printf("impeachment\n");
+        else printf("acusacao arquivada\n");
     }
     return 0;
 }
--- Volume 25 (2500 - 2599)/2540/2540.cpp
+++ Volume 25 (2500 - 2599)/2540/2540.cpp
@@ -1,16 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int n, i, favor = 0, contra = 0;
-    while(scanf("%d", &n) != EOF) {
-        while(n--) {
-            scanf("%d", &i);
-            if(i) favor++;
-            else contra++;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    bool voto;
+    while (cin >> n) {
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            cin >> voto;
+            if (voto) total++;
         }
-        if(favor >= 2*contra) printf("impeachment\n");
-        else printf("acusacao arquivada\n");
-        favor = contra = 0;
+        if (total >= (double)n/3*2) cout << "impeachment\n";
+        else cout << "acusacao arquivada\n";
     }
+    return 0;
 }
