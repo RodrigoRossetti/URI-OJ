078363c338688c8c5622a16c11b49ef9dfb2cd71
RodrigoRossetti <aracknock@gmail.com>
2020-08-30 12:49:48 -0300

    3139 v2

--- Volume 31 (3100 - 3199)/3139/3139.cpp
+++ Volume 31 (3100 - 3199)/3139/3139.cpp
@@ -6,29 +6,23 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int n, m, k, total = 0;
+	int n, m, k, total = 0, soma = 0;
 	cin >> n >> m;
 	m -= n;
 	queue<int> q;
 	for (int i = 0; i < 30; i++) {
 		cin >> k;
+		soma += k;
 		q.push(k);
 	}
 	while (m > 0) {
-		int soma = 0;
-		k = q.front();
+		int k = q.front();
 		q.pop();
-		soma += k;
-		for (int i = 0; i < 29; i++) {
-			k = q.front();
-			q.pop();
-			soma += k;
-			q.push(k);
-		}
 		int media = ceil((double)soma/30);
 		q.push(media);
 		m -= media;
 		total++;
+		soma += media-k;
 	}
 	cout << total << endl;
 	return 0;
