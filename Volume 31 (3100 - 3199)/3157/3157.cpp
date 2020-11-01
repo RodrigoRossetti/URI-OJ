12485e32114c636b4fd1e3893b3a3f5a50c695a4
RodrigoRossetti <aracknock@gmail.com>
2020-10-31 21:21:08 -0300

    3157

---
+++ Volume 31 (3100 - 3199)/3157/3157.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	string s;
+	getline(cin, s);
+	cout << "Nao se refuta ciencia com opiniao\n";
+	return 0;
+}
