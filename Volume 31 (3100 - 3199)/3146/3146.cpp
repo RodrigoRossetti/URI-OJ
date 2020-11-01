713ed00df2d638746d2db72ffc8f9111c112bd2c
RodrigoRossetti <aracknock@gmail.com>
2020-10-31 21:32:23 -0300

    3146

---
+++ Volume 31 (3100 - 3199)/3146/3146.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	double r;
+	cin >> r;
+	cout << fixed << setprecision(2) << 2*3.14*r << endl;
+	return 0;
+}
