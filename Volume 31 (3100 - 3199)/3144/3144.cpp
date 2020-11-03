2ffcb7d025a50ea4348d8d254c739379bc4b3ec4
RodrigoRossetti <aracknock@gmail.com>
2020-11-03 17:08:46 -0300

    3144

---
+++ Volume 31 (3100 - 3199)/3144/3144.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define INF INT_MAX
+
+class grafo {
+	int v;
+	vector<pair<int, int>> *adj;
+	
+	void dfsR(int raiz, vector<bool> &visitado) {
+		visitado[raiz] = true; 
+		cout << raiz << " "; 
+		for (const auto& x : adj[raiz]) 
+			if (!visitado[x.first]) 
+				dfsR(x.first, visitado); 
+	}
+
+public:
+
+	grafo(int v) {
+		this->v = v;
+		adj = new vector<pair<int, int>>[v];
+	}
+
+	void aresta(int u, int w, int k) {
+		adj[u].push_back(make_pair(w, k));
+		adj[w].push_back(make_pair(u, k));
+	}
+
+	void dfs(int raiz) {
+		vector<bool> visitado(v, 0);
+		dfsR(raiz, visitado);
+		cout << endl;
+	}
+
+	vector<int> prim(int raiz) {
+		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+		vector<int> dist(v, INF);
+		vector<bool> visitado(v, 0);
+		pq.push(make_pair(0, raiz));
+		dist[raiz] = 0;
+
+		while (!pq.empty()) {
+			int u = pq.top().second;
+			pq.pop();
+			if (visitado[u] == 1) continue;
+			visitado[u] = 1;
+			for (const auto& w : adj[u]) {
+				if (!visitado[w.first] && dist[w.first] > w.second) {
+					dist[w.first] = w.second;
+					pq.push(make_pair(w.second, w.first));
+				}
+			}
+		}
+		return dist;
+	}
+
+};
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, m, o, u, v, d;	
+	cin >> n >> m >> o;
+	grafo g(n);
+	while (m--) {
+		cin >> u >> v >> d;
+		g.aresta(u-1, v-1, d);
+	}
+	vector<int> arr = g.prim(0);
+	cout << 2*accumulate(arr.begin()+1, arr.end(), 0) << endl;
+	return 0;
+}
