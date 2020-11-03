e3817a2b784e92741daad78a2423c025132372d5
RodrigoRossetti <aracknock@gmail.com>
2020-11-03 16:55:01 -0300

    3134

--- Volume 31 (3100 - 3199)/3134/3134.cpp
+++ Volume 31 (3100 - 3199)/3134/3134.cpp
@@ -1,13 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int read() {
+	int x, r;
+	scanf("%d.%d", &x, &r);
+	return 10*x+r;
+}
+
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	double a, b, c, d;
-	cin >> a >> b >> c >> d;
-	if (a+b+c == d || a+b+d == c || a+c+d == c || b+c+d == a) cout << "YES\n";
+	int a = read(), b = read(), c = read(), d = read();
+	if (a+b+c == d || a+b+d == c || a+c+d == b || b+c+d == a) cout << "YES\n";
 	else if (a+b == c+d || a+c == b+d || a+d == b+c) cout << "YES\n";
-	else cout << "No\n";
+	else cout << "NO\n";
 	return 0;
 }
