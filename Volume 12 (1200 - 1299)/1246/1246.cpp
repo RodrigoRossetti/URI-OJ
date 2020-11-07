8c1352414257c11f7dc3a7cbfb53f70526b82654
RodrigoRossetti <aracknock@gmail.com>
2020-11-06 23:13:32 -0300

    1246

--- Volume 12 (1200 - 1299)/1246/1246.cpp
+++ Volume 12 (1200 - 1299)/1246/1246.cpp
@@ -4,25 +4,39 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int c, n, p, q, total;
+	int c, n, p, q;
 	char t;
 	while (cin >> c >> n) {
-		total = 0;
-		unordered_map<int, int> mapa;
+		vector<int> v(c+1, 0);
+		int total = 0;
 		while (n--) {
 			cin >> t;
 			if (t == 'C') {
 				cin >> p >> q;
-				if (q <= c) {
-					mapa[p] = q;
-					c -= q;
+				bool flag = 0;
+				int pos = 0, qtd = 0;
+				for (int i = 0; i < c; i++) {
+					if (v[i] == 0) {
+						if (qtd == 0) pos = i;
+						qtd++;
+					}
+					else qtd = 0;
+					if (qtd == q) {
+						flag = 1;
+						break;
+					}
+				}
+				if (flag) {
 					total += 10;
+					for (int i = 0; i < q; i++)
+						v[pos+i] = p;
 				}
 			}
 			else if (t == 'S') {
 				cin >> p;
-				c += mapa[p];
-				mapa.erase(p);
+				for (int i = 0; i < c; i++)
+					if (v[i] == p)
+						v[i] = 0;
 			}
 		}
 		cout << total << endl;
