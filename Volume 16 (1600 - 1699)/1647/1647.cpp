d6fa3e016386e4c15a82d0ed1745950a18ada6c8
RodrigoRossetti <aracknock@gmail.com>
2020-11-25 00:04:41 -0300

    1647

--- Volume 16 (1600 - 1699)/1647/1647.cpp
+++ Volume 16 (1600 - 1699)/1647/1647.cpp
@@ -1,17 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define endl '\n'
+#define ll long long
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	int n;
 	while (cin >> n && n) {
-		vector<long long> v(n);
+		vector<ll> v(n);
 		for (int i = 0; i < n; i++) cin >> v[i];
 		for (int i = n-2; i >= 0; i--)
 			for (int j = n-1; j > i; j--)
 				v[i] += v[j];
-		long long soma = 0;
+		ll soma = 0;
 		for (int i = 0; i < n; i++) soma += v[i];
 		cout << soma << endl;
 	}
