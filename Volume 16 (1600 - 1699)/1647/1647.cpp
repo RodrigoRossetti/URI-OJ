1c32dbfa72f97a237dc5fd9a33d3059059698e4e
RodrigoRossetti <aracknock@gmail.com>
2020-11-25 00:03:58 -0300

    1647

--- Volume 16 (1600 - 1699)/1647/1647.cpp
+++ Volume 16 (1600 - 1699)/1647/1647.cpp
@@ -9,8 +9,8 @@ int main() {
 		vector<long long> v(n);
 		for (int i = 0; i < n; i++) cin >> v[i];
 		for (int i = n-2; i >= 0; i--)
-            for (int j = n-1; j > i; j--)
-                v[i] += v[j];
+			for (int j = n-1; j > i; j--)
+				v[i] += v[j];
 		long long soma = 0;
 		for (int i = 0; i < n; i++) soma += v[i];
 		cout << soma << endl;
