032b029493bcb3b71d9c246b06fe796327dcffe4
RodrigoRossetti <aracknock@gmail.com>
2020-11-25 10:24:38 -0300

    1774

--- Volume 17 (1700 - 1799)/1774/1774.cpp
+++ Volume 17 (1700 - 1799)/1774/1774.cpp
@@ -1,48 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class grafo {
+#define endl '\n'
+#define INF 100000
+
+struct grafo {
+
 	int v;
-	vector<pair<int, int>> *adj;
-public:
-	grafo(int v);
-	void aresta(int u, int w, int k);
-	vector<int> prim(int raiz);
+	vector<vector<pair<int, int>>> adj;
+
+	grafo(int v) {
+		this->v = v;
+		adj = vector<vector<pair<int, int>>>(v);
+	}
+
+	void aresta(int u, int w, int k) {
+		adj[u].push_back({w, k});
+		adj[w].push_back({u, k});
+	}
+
+	int prim(int raiz = 0) {
+		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+		vector<int> dist(v, INF);
+		vector<bool> visitado(v, 0);
+		pq.push({0, raiz});
+		dist[raiz] = 0;
+		while (!pq.empty()) {
+			int u = pq.top().second;
+			pq.pop();
+			if (visitado[u]) continue;
+			visitado[u] = 1;
+			for (const auto& w : adj[u]) {
+				if (!visitado[w.first] && dist[w.first] > w.second) {
+					dist[w.first] = w.second;
+					pq.push({w.second, w.first});
+				} 
+			} 
+		} 
+		return accumulate(dist.begin(), dist.end(), 0);
+	}
+
 };
-
-grafo::grafo(int v) {
-	this->v = v;
-	adj = new vector<pair<int, int>>[v];
-}
-
-void grafo::aresta(int u, int w, int k) {
-	adj[u].push_back(make_pair(w, k));
-	adj[w].push_back(make_pair(u, k));
-}
-
-vector<int> grafo::prim(int raiz) { 
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; 
-    vector<int> dist(v, INT_MAX); 
-    vector<int> mst(v, -1); 
-    vector<bool> visitado(v, false); 
-    pq.push(make_pair(0, raiz)); 
-    dist[raiz] = 0; 
-
-    while (!pq.empty()) { 
-        int u = pq.top().second; 
-        pq.pop(); 
-		if(visitado[u] == true) continue;
-        visitado[u] = true; 
-        for (auto w : adj[u]) {  
-            if (!visitado[w.first] && dist[w.first] > w.second) { 
-                dist[w.first] = w.second; 
-                pq.push(make_pair(w.second, w.first)); 
-                mst[w.first] = u; 
-            } 
-        } 
-    } 
-    return dist;
-} 
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -54,9 +52,6 @@ int main() {
 		cin >> v >> w >> p;
 		g.aresta(v-1, w-1, p);
 	}
-	vector<int> dist = g.prim(0);
-	int total = 0;
-	for (int i = 0; i < dist.size(); i++) total += dist[i];
-	cout << total << endl;
+	cout << g.prim() << endl;
 	return 0;
 }
