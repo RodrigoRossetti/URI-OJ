123e494317370cb8a48f289b9352ee22d2fe97d8
RodrigoRossetti <aracknock@gmail.com>
2020-12-01 09:36:41 -0300

    1021

--- Volume 10 (1000 - 1099)/1021/1021.cpp
+++ Volume 10 (1000 - 1099)/1021/1021.cpp
@@ -4,17 +4,17 @@ using namespace std;
 #define endl '\n'
  
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	int out[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-	double value[12] = {100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05, 0.01};
-	double n = 0;
-	cin >> n;
-	for (int i = 0; i < 12; i++) {
-		out[i] = n / value[i];
-		n -= value[i] * out[i];
-	}
-	if (n >= 0.001) out[11]++;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int out[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    double value[12] = {100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05, 0.01};
+    double n = 0;
+    cin >> n;
+    for (int i = 0; i < 12; i++) {
+        out[i] = n / value[i];
+        n -= value[i] * out[i];
+    }
+    if (n >= 0.001) out[11]++;
     cout    << "NOTAS:" << endl
             << out[0] << " nota(s) de R$ 100.00" << endl
             << out[1] << " nota(s) de R$ 50.00" << endl
